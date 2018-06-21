951d47a2a6bf66c13c5642946959e7361499cfe6
Deepa Aravindan <Deepa.Aravindan@unico.com.au>
2018-06-21 11:25:12 +1000

    First commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,20 @@
+/**
+ * Created by daravind on 07/06/2018.
+ */
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+  render(){
+    return (
+      <div className="jumbotron">
+        <h1>Admin</h1>
+        <p>React, Redux and React Router in ES6.</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+      </div>
+    );
+  }
+}
+
+
+export default HomePage;
